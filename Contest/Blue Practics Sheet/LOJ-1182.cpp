96e67e4f6aa626d5fa2d4ab3cb6d79b7d4829969
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-02-06 00:49:56 +0600

    done

---
+++ Contest/Blue Practics Sheet/LOJ-1182.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    for(int i=1; i<=t; i++)
+
+    {
+        int num;
+        cin>>num;
+
+      long long onebit= (__builtin_popcount(num) & 1)
+
+    }
+
+
+    return 0
+}
