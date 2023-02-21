88dac029f2dc1da516e4deb38bb99b5aac4b4647
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-02-21 15:41:48 +0600

    done

--- Contest/insertion.cpp
+++ Contest/insertion.cpp
@@ -18,12 +18,14 @@ int main(){
 
         int j=i;
         
+        
 
        while(j>0 && arr[j-1]>arr[j]){
 
         int temp= arr[j];
         arr[j]=arr[j-1];
         arr[j-1] =temp;
+
 
         j--;
        } 
